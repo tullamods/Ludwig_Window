bf0b984f07f92d18ed0d7d77d75e9084d4f2c059
João Cardoso <jaliborc@gmail.com>
2011-09-05 13:43:33 +0100

    Finally updated to the latest version

--- dropdowns.lua
+++ dropdowns.lua
@@ -4,13 +4,14 @@ local ItemDB = Ludwig('ItemDB')
 
 --[[ Common ]]--
 
-function Dropdowns:Create(name, width, default, initialize, onClick, updateText, parent)
+function Dropdowns:Create(name, width, initialize, click, update, parent)
 	local drop = CreateFrame('Frame', '$parent'..name, parent, 'UIDropDownMenuTemplate')
-	drop.ClickItem = self.ClickItem
-	drop.AddItem = self.AddItem
-	drop.UpdateText = updateText
-	drop.onClick = onClick
-	drop.default = default
+  drop.UpdateText = self.UpdateText
+  drop.AddItem = self.AddItem
+  drop.OnClick = self.OnClick
+
+  drop.update = update
+  drop.click = click
 
 	UIDropDownMenu_Initialize(drop, initialize)
 	UIDropDownMenu_SetWidth(drop, width)
@@ -19,119 +20,106 @@ function Dropdowns:Create(name, width, default, initialize, onClick, updateText,
 	return drop
 end
 
-function Dropdowns:AddItem(text, value, checked, level, hasArrow, ...)
-	local info = UIDropDownMenu_CreateInfo()
-	info.func = self.ClickItem
-	info.hasArrow = hasArrow
-	info.checked = checked
-	info.value = value
-	info.text = text
-	info.owner = self
-
-	for i = 1, select('#', ...) do
-		info['arg' .. i] = select(i, ...)
-	end
-
-	UIDropDownMenu_AddButton(info, level)
+function Dropdowns:UpdateText()
+  _G[self:GetName() .. 'Text']:SetText(self:update())
 end
 
-function Dropdowns:ClickItem(...)
-	self.owner.onClick(self, ...)
-	self.owner:UpdateText()
+function Dropdowns:AddItem(text, value, checked)
+  return {
+    func = self.OnClick,
+    checked = checked,
+    text = text,
+    arg1 = self,
+    arg2 = value
+  }
+end
+
+function Dropdowns:OnClick(self, ...)
+  self:click(...)
+  self:UpdateText()
+  CloseDropDownMenus()
 end
 
 
 --[[ Category ]]--
 
-local subs = {{}, {}, {}}
-local selections = {}
-
 local function category_UpdateText(self)
-	local parent = self:GetParent()
-	local category = parent:GetFilter('category')
-	local text
-
-	if category then
-		local categoryText = {}
-		local subCats = nil
-
-		for level, selectedIndex in ipairs(category) do
-			local i = 1
-			for category, subCategories in ItemDB:IterateCategories(subCats, level) do
-				if i == selectedIndex then
-					subCats = subCategories
-					table.insert(categoryText, category)
-					break
-				end
-				i = i + 1
-			end
-		end
-
-		if #categoryText > 1 then
-			text = ('%s - %s'):format(categoryText[#categoryText - 1], categoryText[#categoryText])
-		else
-			text = categoryText[1]
-		end
-	else
-		text = ALL
-	end
-
-	_G[self:GetName() .. 'Text']:SetText(text)
+  local level = self.names and #self.names or 0
+  if level > 0 then
+    if level > 1 then
+      return ('%s - %s'):format(self.names[2], self.names[1])
+    else
+      return self.names[1]
+    end
+  else
+    return ALL
+  end
 end
 
-local function category_OnClick(self, level)
-	local parent = self.owner:GetParent()
+local function category_Select(self, values)
+  local filter = {}
+  local names = {}
 
-	if self.value ~= ALL then
-		local category = CopyTable(selections)
-		category[level] = self.value
+  if values then
+    for i = #values - 1, 1, -2 do
+      tinsert(filter, values[i])
+    end
+    
+    for i = 2, #values, 2 do
+      tinsert(names, values[i])
+    end
+  end
 
-		parent:SetFilter('category', category, true)
-	else
-		parent:SetFilter('category', nil, true)
-	end
+  self:GetParent():SetFilter('category', values and filter, true)
+  self.names = names
 end
 
-local function category_Initialize(self, level)
-	local category = self:GetParent():GetFilter('category')
-	local level = tonumber(level) or 1
-	if level == 1 then
-		self:AddItem(ALL, ALL, not category)
-	end
+local function category_BuildList(self, filters, list, level, source, ...)
+  local i = 1
 
-	local parentID = UIDROPDOWNMENU_MENU_VALUE
-	local parentChecked = category
-	local parentLevel = level - 1
+  for name, subs in ItemDB:IterateCategories(source, level) do
+    local values = {i, name, ...}
+    local total = #values
+    local checked = filters
 
-	selections[parentLevel] = parentID
-	for i = level, 3 do
-		selections[i] = nil
-	end
+    if checked then
+      for i = 1, total, 2 do
+        if filters[(total + 1 - i) / 2] ~= values[i] then
+          checked = nil
+          break
+        end
+      end
+    end
+    
+    local item = self:AddItem(name, values, checked)
+    if ItemDB:HasSubCategories(subs, level) then
+      item.menuList = {}
+      item.hasArrow = true
+  
+      category_BuildList(self, filters, item.menuList, level + 1, subs, i, name, ...)
+    end
 
-	if category then
-		for i = 1, parentLevel do
-			if category[i] ~= selections[i] then
-				parentChecked = nil
-				break
-			end
-		end
-	end
+    list[i] = item
+    i = i + 1
+  end
 
-	local data = parentID and subs[parentLevel][parentID]
-	local current = parentChecked and category[level]
-	local subs = subs[level]
-	local i = 1
+  return list
+end
 
-	for category, subCategories in ItemDB:IterateCategories(data, level) do
-		self:AddItem(category, i, i == current, level, ItemDB:HasSubCategories(subCategories, level), level)
+local function category_Initialize(self, level, list)
+  local filters = self:GetParent():GetFilter('category')
 
-		subs[i] = subCategories
-		i = i + 1
-	end
+  if not list then
+    list = category_BuildList(self, filters, {}, 1)
+    tinsert(list, 1, self:AddItem(ALL, nil, not filters))
+  end
+
+  EasyMenu_Initialize(self, level, list)
+  collectgarbage()
 end
 
 function Dropdowns:CreateCategory(parent)
-	return self:Create('Category', 200, ALL, category_Initialize, category_OnClick, category_UpdateText, parent)
+	return self:Create('Category', 200, category_Initialize, category_Select, category_UpdateText, parent)
 end
 
 
@@ -141,29 +129,32 @@ local function quality_UpdateText(self)
 	local quality = self:GetParent():GetFilter('quality') or -1
 	if quality ~= -1 then
 		local color = ITEM_QUALITY_COLORS[tonumber(quality)]
-		local text = color.hex .. _G[('ITEM_QUALITY%s_DESC'):format(quality)] .. '|r'
-		_G[self:GetName() .. 'Text']:SetText(text)
+		return color.hex .. _G[('ITEM_QUALITY%s_DESC'):format(quality)] .. '|r'
 	else
-		_G[self:GetName() .. 'Text']:SetText(ALL)
+		return ALL
 	end
 end
 
-local function quality_OnClick(self)
-	self.owner:GetParent():SetFilter('quality', self.value > -1 and tostring(self.value), true)
+local function quality_Select(self, i)
+  self:GetParent():SetFilter('quality', i > -1 and tostring(i), true)
+end
+
+local function quality_AddItem(self, ...)
+  UIDropDownMenu_AddButton(self:AddItem(...))
 end
 
 local function quality_Initialize(self)
 	local quality = tonumber(self:GetParent():GetFilter('quality'))
-	self:AddItem(ALL, -1, not quality)
+  quality_AddItem(self, ALL, -1, not quality)
 
 	for i = 0, #ITEM_QUALITY_COLORS do
 		local color = ITEM_QUALITY_COLORS[i]
 		local text = color.hex .. _G[('ITEM_QUALITY%d_DESC'):format(i)] .. '|r'
 
-		self:AddItem(text, i, quality == i)
+   quality_AddItem(self, text, i, quality == i)
 	end
 end
 
 function Dropdowns:CreateQuality(parent)
-	return self:Create('Quality', 90, -1, quality_Initialize, quality_OnClick, quality_UpdateText, parent)
+	return self:Create('Quality', 90, quality_Initialize, quality_Select, quality_UpdateText, parent)
 end
